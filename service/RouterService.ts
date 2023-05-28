fd5fec3bcdf28774bce98b2dbf616a046bbf8b61
Ayse Gul Eren <ayse.eren@windowslive.com>
2023-05-28 22:27:58 +0300

    RouterService creation and usage

---
+++ service/RouterService.ts
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const apiUrl = "https://localhost:7102/api";
+
+const RouterService = {
+  getItems: async () => {
+    const response = await fetch(`${apiUrl}/GetAll`, { mode: "cors" });
+    const data = await response.json();
+
+    return data;
+  },
+};
+
+export default RouterService;
--- src/pages/Person.tsx
+++ src/pages/Person.tsx
@@ -1,12 +1,20 @@
 import { Button } from "devextreme-react";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import RouterService from "../../service/RouterService";
 
 const Person: React.FC = () => {
-  const sayHelloWorld = () => {
-    alert("Hello");
-  };
+  const [persons, setPersons] = useState();
 
-  return <Button text="Click me" onClick={sayHelloWorld} />;
+  useEffect(() => {
+    const fetchData = async () => {
+      const itemsData = await RouterService.getItems();
+      console.log(itemsData);
+    };
+
+    fetchData();
+  }, []);
+
+  return <Button text="Click me" onClick={() => {}} />;
 };
 
 export default Person;
