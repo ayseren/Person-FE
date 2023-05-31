b9b29823aa1da79f6257a33621da56dc6c53421a
ayseguleren <ayse.eren@windowslive.com>
2023-05-31 22:12:00 +0300

    Person: PersonService usage

--- src/pages/Person.tsx
+++ src/pages/Person.tsx
@@ -1,81 +1,108 @@
 import React, { useEffect, useState } from "react";
 import RouterService from "../../service/RouterService";
-import { DataGrid } from "devextreme-react";
-import { Button, Column, Editing, Summary, TotalItem } from "devextreme-react/data-grid";
+import { DataGrid, Form, Popup, SpeedDialAction, Toolbar } from "devextreme-react";
+import { Column, Editing, Item, Lookup, Pager, Paging } from "devextreme-react/data-grid";
 import "devextreme/dist/css/dx.common.css";
 import "devextreme/dist/css/dx.light.css";
+import CustomStore from "devextreme/data/custom_store";
 import styles from "./Person.module.scss";
 
-enum GENDER {
-  "FEMALE" = "Female",
-  "MALE" = "Male",
-}
-
 type Person = {
-  id: number;
+  id?: number;
   firstName: string;
   middleName?: string;
   lastName: string;
   birthDate: Date;
-  gender: GENDER;
+  gender: string;
   location: string;
 };
 
 const Person: React.FC = () => {
-  const [persons, setPersons] = useState<Person[]>([{ id: 0, firstName: "", lastName: "", birthDate: new Date(), gender: GENDER.FEMALE, location: "" }]);
+  const [lookupDataSource, setLookupDataSource] = useState<{ id: number; name: string }[]>([]);
+
+  let grid: DataGrid<Person, any> | null = null;
+  const genders = [
+    { id: 1, name: "FEMALE" },
+    { id: 2, name: "MALE" },
+  ];
+
+  const addRow = () => {
+    if (grid) {
+      grid.instance.addRow();
+      grid.instance.deselectAll();
+    }
+  };
+
+  const ordersData = new CustomStore({
+    load: () => {
+      return RouterService.getPersons();
+    },
+    insert: async (values: Person) => {
+      const person = {
+        firstName: values.firstName,
+        middleName: values.middleName,
+        lastName: values.lastName,
+        birthDate: values.birthDate,
+        gender: values.gender,
+        location: values.location,
+      };
+
+      return RouterService.addPerson(person);
+    },
+    update: async (key: Person, values: Person) => {
+      if (key.id) {
+        const updatedValues = {
+          lastName: values.lastName ? values.lastName : key.lastName,
+          firstName: values.firstName ? values.firstName : key.firstName,
+          middleName: values.middleName ? values.middleName : key.middleName,
+          birthDate: values.birthDate ? values.birthDate : key.birthDate,
+          gender: values.gender ? values.gender : key.gender,
+          location: values.location ? values.location : key.location,
+        };
+
+        return RouterService.updatePerson(key.id, updatedValues);
+      }
+    },
+    remove: async (key: Person) => {
+      if (key.id) return RouterService.deletePerson(key.id);
+    },
+  });
 
   useEffect(() => {
-    const fetchData = async () => {
-      const itemsData = await RouterService.getPersons();
-      setPersons(itemsData);
-    };
+    RouterService.getPersons().then((persons) => {
+      const uniqueGenders = genders.filter((gender) => persons.some((person: Person) => person.gender === gender.name));
 
-    fetchData();
+      setLookupDataSource(uniqueGenders);
+    });
   }, []);
-
-  const onRowValidating = (e: any) => {
-    const position = e.newData.Position;
-  };
-
-  const onEditorPreparing = (e: any) => {
-    console.log("asfdf", e);
-  };
-
-  const allowDeleting = (e: any) => {
-    console.log(e.row.data.Position);
-  };
-
-  const columns: any = [];
-  persons.forEach((person) => {
-    columns.push(
-      <>
-        <Column type="buttons" width={110}>
-          <Button name="edit" />
-          <Button name="delete" />
-        </Column>
-        <Column dataField={person.id} caption={person.id} />
-        <Column dataField={person.firstName} caption={person.firstName} />
-        <Column dataField={person.middleName} caption={person.middleName} />
-        <Column dataField={person.lastName} caption={person.lastName} />
-        <Column dataField={person.birthDate} caption={person.birthDate} dataType="date" />
-        <Column dataField={person.gender} caption={person.gender} />
-        <Column dataField={person.location} caption={person.location} />
-      </>
-    );
-  });
 
   return (
     <div className={styles.container}>
-      <DataGrid dataSource={persons} showBorders={true} id="personTable" key="Id" onRowValidating={onRowValidating} onEditorPreparing={onEditorPreparing}>
-        <Editing mode="row" useIcons={true} allowUpdating={true} allowDeleting={allowDeleting} />
-        <Column dataField="id" caption="ID" />
+      <SpeedDialAction icon="add" label="Add" index={1} onClick={addRow} />
+
+      <DataGrid
+        dataSource={ordersData}
+        showBorders={true}
+        id="personTable"
+        key="Id"
+        allowColumnReordering={true}
+        ref={(ref) => {
+          grid = ref;
+        }}
+      >
+        <Editing allowUpdating={true} allowDeleting={true} mode="popup">
+          <Form labelLocation="top" />
+          <Popup showTitle={true} title="Row in the editing state" />
+        </Editing>
         <Column dataField="firstName" caption="First Name" />
         <Column dataField="middleName" caption="Middle Name" />
         <Column dataField="lastName" caption="Last Name" />
-        <Column dataField="birthDate" caption="Birth Date" />
-        <Column dataField="gender" caption="Gender" />
+        <Column dataField="birthDate" caption="Birth Date" dataType="date" />
+        <Column dataField="gender" caption="Gender">
+          <Lookup dataSource={lookupDataSource} displayExpr="name" />
+        </Column>
         <Column dataField="location" caption="Location" />
-        {columns}
+        <Paging defaultPageSize={10} />
       </DataGrid>
     </div>
   );
